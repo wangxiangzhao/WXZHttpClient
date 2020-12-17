058fd9d68c638fd7b7b979cf7385c5b81934d20d
小叨 <13269532539@163.com>
2020-12-17 18:55:05 +0800

    修改文件

--- WXZHttpClient/ViewController.m
+++ WXZHttpClient/ViewController.m
@@ -6,9 +6,9 @@
 //
 
 #import "ViewController.h"
-#import "WXZNetWork.h"
+//#import "WXZNetWork.h"
 
-@interface ViewController () <WXZResponseDelegate>
+@interface ViewController ()
 
 @end
 
@@ -16,23 +16,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [WXZNetWorkConfig configWithHost:@"https://app.hotnovelapp.com/" cdn:nil params:nil header:^NSDictionary * _Nullable{
-        return @{};
-    }];
-    UIDevice * currentDevice = [UIDevice currentDevice];
-    NSString * deviceId = [[currentDevice identifierForVendor] UUIDString];
-    WXZRequest *request = [[WXZRequest alloc] initWithPath:@"user/register/" method:@"POST" params:@{@"deviceId" : deviceId}];
-    request.requestSerializerType = WXZRequestSerializerTypeJson;
-    request.responseSerializerType = WXZResponseSerializerTypeJSON;
-    request.delegate = self;
-    [request start];
+//    [WXZNetWorkConfig configWithHost:@"https://app.hotnovelapp.com/" cdn:nil params:nil header:^NSDictionary * _Nullable{
+//        return @{};
+//    }];
+//    UIDevice * currentDevice = [UIDevice currentDevice];
+//    NSString * deviceId = [[currentDevice identifierForVendor] UUIDString];
+//    WXZRequest *request = [[WXZRequest alloc] initWithPath:@"user/register/" method:@"POST" params:@{@"deviceId" : deviceId}];
+//    request.requestSerializerType = WXZRequestSerializerTypeJson;
+//    request.responseSerializerType = WXZResponseSerializerTypeJSON;
+//    request.delegate = self;
+//    [request start];
 }
 
-#pragma mark - WXZResponseDelegate
-
-//请求成功
-- (void)request:(WXZRequest *)request successWithResponse:(id)response {
-    
-}
+//#pragma mark - WXZResponseDelegate
+//
+////请求成功
+//- (void)request:(WXZRequest *)request successWithResponse:(id)response {
+//
+//}
 
 @end
--- WXZNetWork.podspec
+++ WXZNetWork.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "WXZNetWork"
-  spec.version      = "1.0.1"
+  spec.version      = "1.0.2"
   spec.summary      = "AFNetworking的封装"
 
   # This description is used to generate tags and improve search results.
