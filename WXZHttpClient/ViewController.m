91e91a2130a76d3092f39898dfad8b5a3ddfb1f9
小叨 <13269532539@163.com>
2020-12-18 18:05:07 +0800

    修改文件

--- WXZHttpClient/ViewController.m
+++ WXZHttpClient/ViewController.m
@@ -6,13 +6,13 @@
 //
 
 #import "ViewController.h"
-#import "WXZRequestManager.h"
-#import "WXZNetWorkConfig.h"
-#import "AFNetworking.h"
-#import "WXZRequest.h"
-
-#import "WXZBatchRequest.h"
-#import "WXZSerialRequest.h"
+//#import "WXZRequestManager.h"
+//#import "WXZNetWorkConfig.h"
+//#import "AFNetworking.h"
+//#import "WXZRequest.h"
+//
+//#import "WXZBatchRequest.h"
+//#import "WXZSerialRequest.h"
 
 @interface ViewController ()
 
@@ -24,36 +24,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //全局配置
-    [WXZNetWorkConfig configWithHost:@"https://app.hotnovelapp.com/" cdn:nil params:nil header:^NSDictionary * _Nullable{
-        return @{};
-    }];
-    //单个请求
-    UIDevice * currentDevice = [UIDevice currentDevice];
-    NSString * deviceId = [[currentDevice identifierForVendor] UUIDString];
-    WXZRequest *request = [[WXZRequest alloc] initWithPath:@"user/register/" method:@"POST" params:@{@"deviceId" : deviceId}];
-    request.requestSerializerType = WXZRequestSerializerTypeJson;
-    request.responseSerializerType = WXZResponseSerializerTypeJSON;
-    request.delegate = self;
-    [request start];
-    
-    WXZRequest *request1 = [[WXZRequest alloc] initWithPath:@"user/register/" method:@"POST" params:@{@"deviceId" : deviceId}];
-    request.requestSerializerType = WXZRequestSerializerTypeJson;
-    request.responseSerializerType = WXZResponseSerializerTypeJSON;
-    
-    //并行请求
-    [WXZBatchRequest batchRequestWithRequests:@[request, request1] appendDataForm:^FormDataBlock _Nullable(WXZRequest * _Nonnull request) {
-        return nil;
-    } completed:^(NSArray<WXZResponse *> * _Nonnull responses) {
-
-    }];
-    
-    //串行请求
-    [WXZSerialRequest serialRequestWithRequests:@[request, request1] appendDataForm:nil next:^(NSInteger idx, WXZResponse * _Nonnull response, BOOL * _Nonnull stop) {
-        *stop = YES;
-    } completed:^{
-        
-    }];
-    
+//    [WXZNetWorkConfig configWithHost:@"https://app.hotnovelapp.com/" cdn:nil params:nil header:^NSDictionary * _Nullable{
+//        return @{};
+//    }];
+//    //单个请求
+//    UIDevice * currentDevice = [UIDevice currentDevice];
+//    NSString * deviceId = [[currentDevice identifierForVendor] UUIDString];
+//    WXZRequest *request = [[WXZRequest alloc] initWithPath:@"user/register/" method:@"POST" params:@{@"deviceId" : deviceId}];
+//    request.requestSerializerType = WXZRequestSerializerTypeJson;
+//    request.responseSerializerType = WXZResponseSerializerTypeJSON;
+//    request.delegate = self;
+//    [request start];
+//    
+//    WXZRequest *request1 = [[WXZRequest alloc] initWithPath:@"user/register/" method:@"POST" params:@{@"deviceId" : deviceId}];
+//    request.requestSerializerType = WXZRequestSerializerTypeJson;
+//    request.responseSerializerType = WXZResponseSerializerTypeJSON;
+//    
+//    //并行请求
+//    [WXZBatchRequest batchRequestWithRequests:@[request, request1] appendDataForm:^FormDataBlock _Nullable(WXZRequest * _Nonnull request) {
+//        return nil;
+//    } completed:^(NSArray<WXZResponse *> * _Nonnull responses) {
+//
+//    }];
+//    
+//    //串行请求
+//    [WXZSerialRequest serialRequestWithRequests:@[request, request1] appendDataForm:nil next:^(NSInteger idx, WXZResponse * _Nonnull response, BOOL * _Nonnull stop) {
+//        *stop = YES;
+//    } completed:^{
+//        
+//    }];
+//    
     NSLog(@"啊哈哈");
 }
 
--- WXZNetWork.podspec
+++ WXZNetWork.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "WXZNetWork"
-  spec.version      = "1.0.3"
+  spec.version      = "1.0.0"
   spec.summary      = "AFNetworking的封装"
 
   # This description is used to generate tags and improve search results.
